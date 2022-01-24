f86ac0af8cc14d4662920db1cf641a2d8606c3dc
MikHail854 <vip.moneta95@mail.ru>
2022-01-24 11:33:19 +0700

    added method init()

--- src/test/java/ru/chigurov/LinkedListCustomTest.java
+++ src/test/java/ru/chigurov/LinkedListCustomTest.java
@@ -1,5 +1,6 @@
 package ru.chigurov;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LinkedListCustomTest {
 
     InitMyList list;
+
+    @BeforeEach
+    public void init(){
+        list = new InitMyList();
+        list.initList();
+        assertNotNull(list);
+    }
 
     @Test
     public void addFirstElementTest() {
@@ -36,9 +44,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void addElementByIndexToMiddleTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.add(2, "000");
         assertEquals(6, list.linkedListCustom.size());
         assertEquals("000", list.linkedListCustom.get(2));
@@ -46,9 +51,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void addElementByIndexToEndTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.add(list.linkedListCustom.size(), "000");
         assertEquals(6, list.linkedListCustom.size());
         assertEquals("000", list.linkedListCustom.get(list.linkedListCustom.size() - 1));
@@ -56,18 +58,12 @@ public class LinkedListCustomTest {
 
     @Test
     public void getTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         String result = list.linkedListCustom.get(3);
         assertEquals("444", result);
     }
 
     @Test
     public void removeByToBeginningIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.remove(0);
         assertEquals(4, list.linkedListCustom.size());
         assertFalse(list.linkedListCustom.contains("111"));
@@ -75,9 +71,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void removeByMiddleIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.remove(3);
         assertEquals(4, list.linkedListCustom.size());
         assertFalse(list.linkedListCustom.contains("444"));
@@ -85,9 +78,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void removeByEndIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.remove(list.linkedListCustom.size() - 1);
         assertEquals(4, list.linkedListCustom.size());
         assertFalse(list.linkedListCustom.contains("555"));
@@ -95,25 +85,16 @@ public class LinkedListCustomTest {
 
     @Test
     public void containsFalseTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         assertFalse(list.linkedListCustom.contains("9999"));
     }
 
     @Test
     public void containsTrueTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         assertTrue(list.linkedListCustom.contains("444"));
     }
 
     @Test
     public void setToBeginningIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.set(0, "000");
         assertTrue(list.linkedListCustom.contains("000"));
         assertEquals("000", list.linkedListCustom.get(0));
@@ -121,9 +102,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void setToMiddleIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.set(3, "999999");
         assertTrue(list.linkedListCustom.contains("999999"));
         assertEquals("999999", list.linkedListCustom.get(3));
@@ -132,9 +110,6 @@ public class LinkedListCustomTest {
 
     @Test
     public void setToEndIndexTest() {
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.set(list.linkedListCustom.size() - 1, "777");
         assertTrue(list.linkedListCustom.contains("777"));
         assertEquals("777", list.linkedListCustom.get(list.linkedListCustom.size() - 1));
@@ -142,17 +117,11 @@ public class LinkedListCustomTest {
 
     @Test
     public void sizeTest(){
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         assertEquals(5, list.linkedListCustom.size());
     }
 
     @Test
     public void clearTest(){
-        list = new InitMyList();
-        list.initList();
-        assertNotNull(list);
         list.linkedListCustom.clear();
         assertEquals(0, list.linkedListCustom.size());
     }
